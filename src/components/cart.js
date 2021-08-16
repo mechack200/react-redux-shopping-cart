c2e992a745d920ca9ee63cc60c661ee0844f5baa
mechack200 <taiwogboyegun@gmmail.com>
2021-08-16 21:13:44 +0100

    checkout form added

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,62 +1,142 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { formatCurrency } from '../util';
 import './cart.css';
+export default class cart extends Component {
+	constructor() {
+		super();
+		this.state = {
+			checkout: false,
+			name: '',
+			email: '',
+			address: '',
+		};
+	}
 
-const Cart = ({ cartItems, removeItemFromCart }) => {
-	return (
-		<div>
-			{cartItems.length === 0 ? (
-				<div className="cart-header header"> no item in cart </div>
-			) : (
-				<div className="cart-header header">
-					You have {cartItems.length} item
-					{cartItems.length === 1 ? '' : 's'} in your cart
-				</div>
-			)}
+	createOrder = (e) => {
+		// e.preventDefault();
+		const { name, address, email } = this.state;
+		const { creatOrder } = this.props;
+		const order = {
+			cartItems: this.props.cartItems,
+			name,
+			address,
+			email,
+		};
+		creatOrder(order);
+	};
+
+	handleInputChange = (e) => {
+		const value = e.target.value;
+		this.setState({
+			[e.target.name]: value,
+		});
+	};
+
+	render() {
+		const { cartItems, removeItemFromCart } = this.props;
+		return (
 			<div>
-				<div className="cart">
-					<ul className="cart-items">
-						{cartItems.map((item) => (
-							<li key={item._id}>
-								<div>
-									<img src={item.image} alt={item._id} />
-								</div>
-								<div>
-									<div className="title">{item.title}</div>
-									<div className="right">
-										{formatCurrency(item.price)} X {item.count}
-										<button
-											className="button"
-											onClick={() => removeItemFromCart(item)}
-										>
-											Remove from cart
-										</button>
-									</div>
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
-				{/* if cart item is not equals to o then  */}
-				{cartItems.length !== 0 && (
-					<div className="cart">
-						<div className="total">
-							<div>
-								total:{' '}
-								{formatCurrency(
-									cartItems.reduce(
-										(acc, current) => acc + current.price * current.count,
-										0
-									)
-								)}
-							</div>
-							<button className="button primary">Proceed </button>
-						</div>
+				{cartItems.length === 0 ? (
+					<div className="cart-header header"> no item in cart </div>
+				) : (
+					<div className="cart-header header">
+						You have {cartItems.length} item
+						{cartItems.length === 1 ? '' : 's'} in your cart
 					</div>
 				)}
+				<div>
+					<div className="cart">
+						<ul className="cart-items">
+							{cartItems.map((item) => (
+								<li key={item._id}>
+									<div>
+										<img src={item.image} alt={item._id} />
+									</div>
+									<div>
+										<div className="title">{item.title}</div>
+										<div className="right">
+											{formatCurrency(item.price)} X {item.count}
+											<button
+												className="button"
+												onClick={() => removeItemFromCart(item)}
+											>
+												Remove from cart
+											</button>
+										</div>
+									</div>
+								</li>
+							))}
+						</ul>
+					</div>
+					{/* if cart item is not equals to o then  */}
+					{cartItems.length !== 0 && (
+						<div>
+							<div className="cart">
+								<div className="total">
+									<div>
+										total:{' '}
+										{formatCurrency(
+											cartItems.reduce(
+												(acc, current) => acc + current.price * current.count,
+												0
+											)
+										)}
+									</div>
+									<button
+										onClick={() => {
+											this.setState({ checkout: true });
+										}}
+										className="button primary"
+									>
+										Proceed
+									</button>
+								</div>
+							</div>
+							{this.state.checkout && (
+								<div className="cart">
+									<form onSubmit={this.createOrder}>
+										<ul className="form-container">
+											<li>
+												<label>Email</label>
+												<input
+													name="email"
+													type="email"
+													require="true"
+													onChange={this.handleInputChange}
+												/>
+											</li>
+											<li>
+												<label>Name</label>
+												<input
+													name="name"
+													type="text"
+													require="true"
+													onChange={this.handleInputChange}
+												/>
+											</li>
+											<li>
+												<label>AddresSs</label>
+												<input
+													name="address"
+													type="text"
+													require="true"
+													onChange={this.handleInputChange}
+												/>
+											</li>
+											<li>
+												<button type="submit" className="button primary">
+													{' '}
+													checkout
+												</button>
+											</li>
+										</ul>
+									</form>
+								</div>
+							)}
+						</div>
+					)}
+				</div>
 			</div>
-		</div>
-	);
-};
-
-export default Cart;
+		);
+	}
+}
